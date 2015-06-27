cff01d2a6e57fbdc53c10ec5671aaf47993cb761
ticklemynausea <emailoftheyear@gmail.com>
2015-06-27 18:30:51 +0100

    iframe selector for custom dynmap css

--- js/sop.js
+++ js/sop.js
@@ -1,6 +1,6 @@
 // bypass same origin policy on *.minecraft.pt
 document.domain = "minecraft.pt";
 
-jQuery("iframe").load(function(el) {
+jQuery("iframe.dynmap").load(function(el) {
     this.contentWindow.$('head').append('<link rel="stylesheet" type="text/css" href="//panel.minecraft.pt/css/dynmap/clean.css">');
 });
