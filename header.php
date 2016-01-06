64148ebe5f56593730b6241c3989413ad0e58b06
Mário C <igotbugs@ticklemynausea.net>
2016-01-06 00:45:39 +0000

    meta tags

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<head>
 		
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+		   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+		   <meta name="keywords" content="minecraft mine portugal minept factions videogames multiplayer">
+		   <meta name="description" content="Website da Comunidade Minecraft Portugal">
 																		
 		<title><?php wp_title('|', true, 'right'); ?></title>
 				
